721e2f1094255b1cddf4609a12cc5204a8049308
zeynepkrmc <m.zeynephnm@gmail.com>
2023-07-11 08:25:40 +0300

    functions added

--- slider_script.js
+++ slider_script.js
@@ -30,9 +30,20 @@ var models = [
 
     }
 ];
-
-var index=1;
-
+/**bu ndex ile hangi slide  aldık onu belirledik */
+var index=0;
+/**model objesi üzerindeki elemanları teker teker slide a aktardık */
 document.querySelector('.card-title').textContent = models[index].name;
 document.querySelector('.card-img-top').setAttribute('src',models[0].image);
 document.querySelector('.card-link').setAttribute('href',models[index].link);
+
+/**click event */
+document.querySelector('.fa-arrow-circle-left').addEventListener('click', function(){
+    index--;
+    console.log(index);
+});
+document.querySelector('.fa-arrow-circle-left').addEventListener('click', function(){
+    index++;
+    console.log(index);
+});
+
